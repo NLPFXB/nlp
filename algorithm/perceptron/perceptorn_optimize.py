cd109160b3e5c656e37ce41bb919615c2426c056
fengxiaobo <fengxiaobo>
2018-08-11 14:04:32 +0800

    感知机~
    感知机更新

--- algorithm/perceptron/perceptorn_optimize.py
+++ algorithm/perceptron/perceptorn_optimize.py
@@ -5,5 +5,13 @@
 '''
 
 import numpy as np
-
+#计算gram矩阵
 print(np.outer([1,2,3],[4,5,6,7]))
+
+def compute_garms(data_X):
+    """
+    :param data_X:  输入变量
+    :return: 计算变量
+    """
+    for line in data_X:
+        line
